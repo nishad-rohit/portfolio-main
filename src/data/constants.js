5933308a931123d2a2ffd998e54366f9270e1906
rohitnishad <rohitn@voizzit.com>
2025-01-22 19:19:27 +0400

    adding portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,200 @@
+
+
+export const Bio = {
+  name: "Rohit Nishad",
+  roles: ["Programmer", "Java Developer", "Senior Software Engineer", "Team Leader"],
+  description:
+    "Experienced Java Developer with 6+ years of expertise in building scalable and high-performance applications using Java (8, 11, 17), Spring Boot, and Microservices architecture. Proficient in designing robust systems with Hibernate, Redis, and AWS services. Skilled in implementing CI/CD pipelines using tools like JFrog and delivering innovative, efficient solutions for Banking and E-commerce domains. Adept at integrating complex architectures and optimizing application performance to drive business success.",
+  github: "https://github.com/Nishad1999",
+  resume:
+    "https://drive.google.com/file/d/1fDK3S4SMI1t-AilvU0wLy99A9oUbSTMG/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rohit-nishad-9a5301294/",
+  insta:
+    "https://instagram.com/__rohit_nishad?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D",
+};
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "JavaScript",
+        image:
+          "https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg",
+      },
+      {
+        name: "Java 8",
+        image: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+      },
+      {
+        name: "Java 11",
+        image: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+      },
+      {
+        name: "Java 17",
+        image: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+      }
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+       
+      {
+        name: "React.js",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/440px-React-icon.svg.png",
+      } 
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      
+      {
+        name: "Spring Boot",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "Microservices",
+        image: "https://cdn.iconscout.com/icon/premium/png-512-thumb/microservices-1660470-1408971.png",
+      },
+      {
+        name: "Hibernate",
+        image: "https://www.vectorlogo.zone/logos/hibernate/hibernate-icon.svg",
+      },
+      { name: "Socket.IO", image: "https://socket.io/images/logo.svg" },
+      {
+        name: "RabbitMQ",
+        image:
+          "https://dyltqmyl993wv.cloudfront.net/assets/stacks/rmq-messaging-topology-operator/img/rmq-messaging-topology-operator-stack-220x234.png",
+      },
+    ],
+  },
+  {
+    title: "Databases & Cache",
+    skills: [
+      {
+        name: "PostgreSQL",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Redis",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Redis_logo.svg/400px-Redis_logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "DevOps & Tools",
+    skills: [
+      {
+        name: "Git",
+        image: "https://git-scm.com/images/logos/downloads/Git-Logo-2Color.svg",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://www.docker.com/wp-content/uploads/2022/03/Moby-logo.png",
+      },
+      {
+        name: "AWS",
+        image: "https://a0.awsstatic.com/main/images/logos/aws_logo.png",
+      },
+      {
+        name: "JFrog",
+        image: "https://www.vectorlogo.zone/logos/jfrog/jfrog-icon.svg",
+      }, 
+      {
+        name: "JIRA",
+        image: "https://www.vectorlogo.zone/logos/atlassian_jira/atlassian_jira-icon.svg",
+      },
+      {
+        name: "CI/CD",
+        image: "https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg", // Jenkins as an example of CI/CD tool
+      },
+      {
+        name: "DigitalOcean",
+        image: "https://getlogo.net/wp-content/uploads/2020/10/digitalocean-logo-vector.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D4D0BAQGZW5wJindNgw/company-logo_200_200/company-logo_200_200/0/1696677600640/voizzitofficial_logo?e=1741219200&v=beta&t=A61msFVJo1sXYh3tIy89CUi5dBtLZmOn9FbdBqtdEeg",
+    role: "Senior Java Developer",
+    company: "Voizzit",
+    date: "August 2023 - Present",
+    desc: "Led the design and development of a feature-rich e-commerce platform, utilizing expertise in Java and system architecture to deliver innovative solutions that enhanced business operations and customer engagement.",
+    skills: ["Spring Boot","Microservice", "PostgreSQL", "AWS","S3", "Redis", "Docker","CI/CD","JFrog","HTML","CSS"],
+  },
+  {
+    id: 1,
+    //img: "https://media.licdn.com/dms/image/v2/C510BAQE5jbbFEAeWFA/company-logo_200_200/company-logo_200_200/0/1631369215758?e=1741219200&v=beta&t=Tql8I-3lg58bYaiCv-kJp2XllcJ2S36qpAgZvqP6e2E",
+    role: "Senior Software Engineer",
+    company: "WorldLine",
+    date: "August 2022 - July 2023",
+    desc: "Developed a microservice subscription portal enabling various sectors to access services securely via tokens. Built a maintenance management application for oil and gas equipment with a multi-level approval system.",
+    skills: ["Spring Boot","Microservice", "PostgreSQL","Hibernate", "AWS", "HTML", "CSS"],
+  },
+  {
+    id: 2,
+   // img: "https://media.licdn.com/dms/image/v2/C560BAQHOBmJs5EihDA/company-logo_200_200/company-logo_200_200/0/1630644305985/oneture_logo?e=1741219200&v=beta&t=Djg-YAzFY-875gDwBabhXWsU7hUjB1y7m6Yhu6ns4Io",
+    role: "Software Engineer",
+    company: "Analytical fox Software",
+    date: "February 2022 - August 2022",
+    desc: "Developed ATM operation management applications serving 60,000+ daily users. Built mobile and web applications using TypeScript, React Native, Angular, and Redux.",
+    skills: ["java8", "Spring Boot", "Microservice", "Hibernate", "MySql","AWS"],
+  },
+  {
+    id: 3,
+    // img: "https://drive.google.com/file/d/1KLaR54U1fpqxyG3zlBTYV6zyKalrr1-4/view?usp=sharing",
+    role: "Software Engineer",
+    company: "NDML",
+    date: "April 2021 - February 2022",
+    desc: "Developed an E-commerce platform and a social media application with features similar to Instagram and Clubhouse. Created cross-platform caregiving service applications using React Native and Node.js.",
+    skills: ["java8", "Spring Boot", "Microservice", "Hibernate", "PostgreSql","HTML", "CSS"],
+  },
+  {
+    id: 3,
+    // img: "https://media.licdn.com/dms/image/C560BAQHOBmJs5EihDA/company-logo_200_200/...",
+    role: "Executive Developer",
+    company: "Tapup",
+    date: "February 2019 - April 2021",
+    desc: "Developed an E-commerce platform and a social media application with features similar to Instagram and Clubhouse. Created cross-platform caregiving service applications using React Native and Node.js.",
+    skills: ["java8", "Spring Boot", "Mongodb", "DigitalOcean","HTML", "CSS"],
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://image-static.collegedunia.com/public/college_data/images/logos/1709543727PHCETLogo21.png?h=71.17&w=71.17&mode=stretch",
+    school: "Mumbai University: SIWS College",
+    date: "2019 - 2021",
+    desc: "M.Sc in Information Technology",
+    degree: "Master of Science",
+  },
+  {
+    id: 1,
+     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwvD5doh6rt92H8YOhvDd0jFahj2Im6I36JnZZNfY&usqp=CAE&s",
+    school: "Mumbai University: BGPS College",
+    date: "Apr 2016 - Apr 2019",
+    desc: "Bachelor in Information Technology",
+    degree: "Bachelor of Science",
+  },
+];
