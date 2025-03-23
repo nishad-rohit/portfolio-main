941fc7cf4b319e295d768d3ecd88024517d94170
Nishad1999 <rohitkoli465@gmail.com>
2025-03-24 00:36:04 +0400

    adding go and updating resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
     "Experienced Java Developer with 6+ years of expertise in building scalable and high-performance applications using Java (8, 11, 17), Spring Boot, and Microservices architecture. Proficient in designing robust systems with Hibernate, Redis, and AWS services. Skilled in implementing CI/CD pipelines using tools like JFrog and delivering innovative, efficient solutions for Banking and E-commerce domains. Adept at integrating complex architectures and optimizing application performance to drive business success.",
   github: "https://github.com/Nishad1999",
   resume:
-    "https://drive.google.com/file/d/1UHeFMEMb1Tf_bm147N_iW6WmGHiWLIKg/view?usp=drive_link",
+    "https://drive.google.com/file/d/1wG9Paqr3awF7ywUeZgHZsxpU4Jg7lhW5/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/rohit-nishad-9a5301294/",
   insta:
     "https://instagram.com/__rohit_nishad?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D",
@@ -33,6 +33,10 @@ export const skills = [
       {
         name: "Java 17",
         image: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+      },
+      {
+        name: "Go",
+        image: "https://go.dev/blog/go-brand/Go-Logo/PNG/Go-Logo_Blue.png",
       }
     ],
   },
